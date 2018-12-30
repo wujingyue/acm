aa68908fcf0e9006972a38fa49e56053b9dbf3e0
Jingyue Wu <wujingyue@gmail.com>
2018-12-29 23:24:25 -0800

    Replace map with array.
    This version is much faster because it replaces the map with array.
    
    The key of the array though needs to be the ID of a permutation rather
    than the decimal format of a permutation. Otherwise, the array would be
    too large to fit into memory.

--- poj/1077.cc
+++ poj/1077.cc
@@ -1,11 +1,11 @@
 #include <iostream>
-#include <map>
 #include <queue>
 
 using namespace std;
 
 const int n = 3;
 int power_of_tens[n * n];
+int factorial[n * n + 1];
 
 int FindZero(int b) {
   for (int i = 0; i < n * n; ++i) {
@@ -24,11 +24,29 @@ int Swap(int b, int zero_pos, int neighbor_pos) {
   return b + DigitAtIndex(b, neighbor_pos) * (power_of_tens[zero_pos] - power_of_tens[neighbor_pos]);
 }
 
-bool BFS(int start, int end, map<int, pair<int, char> >* pre) {
+int Compress(int b) {
+  int compressed = 0;
+  int seen_digits = 0;
+  for (int i = 0; i < n * n; ++i) {
+    int digit = b % 10;
+    int num_unseen_digits_before = 0;
+    for (int j = 0; j < digit; ++j) {
+      if ((seen_digits & (1 << j)) == 0) {
+        num_unseen_digits_before++;
+      }
+    }
+    compressed += num_unseen_digits_before * factorial[n * n - 1 - i];
+    seen_digits |= (1 << digit);
+    b /= 10;
+  }
+  return compressed;
+}
+
+bool BFS(int start, int end, vector<pair<int, char> >* pre) {
   queue<int> q;
 
   q.push(start);
-  (*pre)[start] = make_pair(start, ' ');
+  (*pre)[Compress(start)] = make_pair(start, ' ');
 
   pair<int, char> neighbors[4];
   while (!q.empty()) {
@@ -58,11 +76,11 @@ bool BFS(int start, int end, map<int, pair<int, char> >* pre) {
     for (int i = 0; i < num_neighbors; ++i) {
       int neighbor = neighbors[i].first;
       char op = neighbors[i].second;
-      if (pre->count(neighbor)) {
-        continue;
+      int compressed = Compress(neighbor);
+      if ((*pre)[compressed].first == -1) {
+        q.push(neighbor);
+        (*pre)[compressed] = make_pair(b, op);
       }
-      q.push(neighbor);
-      (*pre)[neighbor] = make_pair(b, op);
     }
   }
   return false;
@@ -72,6 +90,10 @@ int main() {
   power_of_tens[0] = 1;
   for (int i = 1; i < n * n; ++i) {
     power_of_tens[i] = power_of_tens[i - 1] * 10;
+  }
+  factorial[0] = 1;
+  for (int i = 1; i <= n * n; ++i) {
+    factorial[i] = factorial[i - 1] * i;
   }
 
   int start = 0;
@@ -89,7 +111,7 @@ int main() {
     end += power_of_tens[i] * (i + 1);
   }
 
-  map<int, pair<int, char> > pre;
+  vector<pair<int, char> > pre(factorial[n * n], make_pair(-1, ' '));
   if (!BFS(start, end, &pre)) {
     cout << "unsolvable" << endl;
     return 0;
@@ -98,8 +120,9 @@ int main() {
   vector<char> ops;
   int b = end;
   while (b != start) {
-    ops.push_back(pre[b].second);
-    b = pre[b].first;
+    int compressed = Compress(b);
+    ops.push_back(pre[compressed].second);
+    b = pre[compressed].first;
   }
   for (int i = (int)ops.size() - 1; i >= 0; --i) {
     cout << ops[i];
