a3f2272719f276eed7884d01360b41d7f79959cd
Jingyue Wu <wujingyue@gmail.com>
2020-05-30 14:51:50 -0700

    Some cleanups.
    1. Compute m[i][j] in one pass.
    2. Compute num_entries on demand.

--- usaco/open20/sprinklers2.cc
+++ usaco/open20/sprinklers2.cc
@@ -1,6 +1,6 @@
+#include <algorithm>
 #include <cassert>
 #include <fstream>
-#include <iostream>
 #include <string>
 #include <vector>
 
@@ -17,14 +17,6 @@ class Solution {
 
   int NumPossibilities(const vector<string>& a) {
     const int n = a.size();
-    vector<int> num_empties(n);
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-        if (a[i][j] == '.') {
-          num_empties[i]++;
-        }
-      }
-    }
 
     // m[i][j] represents the number of possibilities when the divide in row i
     // is at column j. The divide of a row is defined as the column of the
@@ -33,30 +25,34 @@ class Solution {
     // sprinklers. The A cells in column j..n-1 are not necessarily covered by A
     // sprinklers.
     vector<int> m(n + 1);
-    m[0] = LeftShift(1, num_empties[0]);
-    for (int j = 1; j <= n; j++) {
-      if (a[0][j - 1] == '.') {
-        m[j] = LeftShift(1, num_empties[0] - 1);
+    {
+      int num_empties = count(a[0].begin(), a[0].end(), '.');
+      m[0] = LeftShift(1, num_empties);
+      for (int j = 1; j <= n; j++) {
+        if (a[0][j - 1] == '.') {
+          m[j] = LeftShift(1, num_empties - 1);
+        }
       }
     }
 
-    for (int i = 0; i + 1 < n; i++) {
+    for (int i = 1; i < n; i++) {
+      int num_empties = count(a[i].begin(), a[i].end(), '.');
       for (int j = n; j >= 0; j--) {
-        if (m[j] == 0) {
+        m[j] = LeftShift(m[j], num_empties);
+        if (j == 0 || a[i][j - 1] == 'W') {
           continue;
         }
-        for (int j2 = j + 1; j2 <= n; j2++) {
-          if (a[i][j] == 'W' || a[i + 1][j2 - 1] == 'W') {
-            continue;
-          }
-          int e = num_empties[i + 1] - 2;
-          if (e < 0) {
-            assert(e == -1);
-          }
-          m[j2] = (m[j2] + (e == -1 ? DivideByTwo(m[j]) : LeftShift(m[j], e))) %
-                  kModulo;
+        if (num_empties < 2) {
+          assert(num_empties == 1);
         }
-        m[j] = LeftShift(m[j], num_empties[i + 1]);
+        for (int j2 = 0; j2 < j; j2++) {
+          if (a[i - 1][j2] == '.') {
+            m[j] = (m[j] + (num_empties == 1
+                                ? DivideByTwo(m[j2])
+                                : LeftShift(m[j2], num_empties - 2))) %
+                   kModulo;
+          }
+        }
       }
     }
 
@@ -66,8 +62,7 @@ class Solution {
         possibilities = (possibilities + DivideByTwo(m[j])) % kModulo;
       }
     }
-    possibilities = (possibilities + m[n]) % kModulo;
-    return possibilities;
+    return (possibilities + m[n]) % kModulo;
   }
 
  private:
