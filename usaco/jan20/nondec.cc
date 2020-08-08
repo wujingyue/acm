3d969e3d92c52d8a373a47e7c22ff7efe01955fb
Jingyue Wu <wujingyue@gmail.com>
2020-08-07 22:10:52 -0700

    Store only the rightmost column.

--- usaco/jan20/nondec.cc
+++ usaco/jan20/nondec.cc
@@ -138,43 +138,62 @@ class Solution {
   Solution(vector<int> a, const int k) {
     // m[i][j] = m[i-1][j] + (m[i-1][a[i-1]] if j >= a[i])
     const int n = a.size();
-    prefix_product_.reserve(n + 1);
-    prefix_product_.push_back(Matrix::One(k));
+    prefix_product_rightmost_column_.resize(n + 1);
+    flattened_inverse_prefix_product_.resize(n + 1);
+
+    Matrix prefix_product = Matrix::One(k);
+    prefix_product_rightmost_column_[0] = RightmostColumn(prefix_product);
+    flattened_inverse_prefix_product_[0] = Flatten(prefix_product);
+
     for (int i = 1; i <= n; i++) {
       Matrix transfer = Matrix::One(k);
       for (int y = a[i - 1]; y < k; y++) {
         transfer[a[i - 1]][y]++;
       }
-      prefix_product_.push_back(prefix_product_[i - 1] * transfer);
-    }
-
-    flattened_inverse_prefix_product_.reserve(n + 1);
-    for (int i = 0; i <= n; i++) {
-      Matrix inverse_prefix_product = prefix_product_[i].Inverse();
-      vector<int> flattened(k);
-      for (int x = 0; x < k; x++) {
-        for (int y = 0; y < k; y++) {
-          flattened[y] = (flattened[y] + inverse_prefix_product[x][y]) % kModulo;
-        }
-      }
-      flattened_inverse_prefix_product_.push_back(flattened);
+      prefix_product = prefix_product * transfer;
+      prefix_product_rightmost_column_[i] = RightmostColumn(prefix_product);
+      flattened_inverse_prefix_product_[i] = Flatten(prefix_product.Inverse());
     }
   }
 
   int NumNonDecreasing(const int left, const int right) {
-    const vector<int>& v = flattened_inverse_prefix_product_[left];
-    const Matrix& m = prefix_product_[right + 1];
-    const int k = v.size();
+    const vector<int>& v1 = flattened_inverse_prefix_product_[left];
+    const vector<int>& v2 = prefix_product_rightmost_column_[right + 1];
+    assert(v1.size() == v2.size());
+
+    const int k = v1.size();
     int sum = 0;
     for (int x = 0; x < k; x++) {
-      sum = (sum + (long long)v[x] * m[x][k - 1] % kModulo) % kModulo;
+      sum = (sum + (long long)v1[x] * v2[x] % kModulo) % kModulo;
     }
     return sum;
   }
 
  private:
-  vector<Matrix> prefix_product_;
-  vector<vector<int>> flattened_inverse_prefix_product_;
+  static vector<int> Flatten(const Matrix& m) {
+    const int n = m.size();
+    vector<int> flattened(n);
+    for (int y = 0; y < n; y++) {
+      int sum = 0;
+      for (int x = 0; x < n; x++) {
+        sum = (sum + m[x][y]) % kModulo;
+      }
+      flattened[y] = sum;
+    }
+    return flattened;
+  }
+
+  static vector<int> RightmostColumn(const Matrix& m) {
+    const int n = m.size();
+    vector<int> rightmost_column(n);
+    for (int x = 0; x < n; x++) {
+      rightmost_column[x] = m[x][n - 1];
+    }
+    return rightmost_column;
+  }
+
+  vector<vector<int> > prefix_product_rightmost_column_;
+  vector<vector<int> > flattened_inverse_prefix_product_;
 };
 
 int main() {
