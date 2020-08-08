8710c141758627cfdcbe8e315d13bb18da4c8bb1
Jingyue Wu <wujingyue@gmail.com>
2020-08-07 22:44:54 -0700

    Time the execution.

--- usaco/run_tests.py
+++ usaco/run_tests.py
@@ -7,7 +7,7 @@ end = int(sys.argv[3])
 
 out_filename = '{}.out'.format(prob)
 for i in range(start, end + 1):
-  print(f'Running test case {i}...')
+  print(f'\nRunning test case {i}...')
   subprocess.check_call(f'cp {i}.in {prob}.in', shell=True)
-  subprocess.check_call(f'./{prob}.exe', shell=True)
+  subprocess.check_call(f'time ./{prob}.exe', shell=True)
   subprocess.check_call(f'diff -qruN {prob}.out {i}.out', shell=True)
